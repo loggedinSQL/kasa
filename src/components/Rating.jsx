0969cea7d49aad0a083027ce22ba8e10f0b740c2
driedfruits <m.allancouchy@gmail.com>
2022-11-22 13:58:39 +0100

    adding comments

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,18 +2,18 @@ import redstar from '../assets/redstar.svg'
 import graystar from '../assets/graystar.svg'
 
 const Rating = ({ score }) => {
-    const rating = []
+    const rating = [] // création du tableau rating
     let result
 
-    for (let i = 0; i < score; i++) {
+    for (let i = 0; i < score; i++) { // on intègre les étoiles rouges dans le tableau en fonction de la note du logement
         rating.push(<img src={redstar} alt='full star' className='stars' key={rating.toString([i])}/>)
     }
 
-    if (score < 5 ) {
+    if (score < 5 ) { // on vérifie si la note est inférieure à 5
         result = 5 - score
     }
 
-    for (let i = 0; i < result; i++) {
+    for (let i = 0; i < result; i++) { // si score < 5, on intègre les étoiles grises
         rating.push(<img src={graystar} alt='empty star' className='stars' key={rating.toString([i])} />)
     }
 
