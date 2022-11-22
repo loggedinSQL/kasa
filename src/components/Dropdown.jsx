76b5790d0ad711be89b5479827e1492791727d91
driedfruits <m.allancouchy@gmail.com>
2022-11-22 14:10:20 +0100

    adding comments

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react' // importer le composant useState de React
 import chevron from '../assets/vector.svg'
 
 const Dropdown = ({ title, content }) => {
