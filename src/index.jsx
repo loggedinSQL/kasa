2275724da75b23dc0ea058489064b39fdedacbb9
driedfruits <m.allancouchy@gmail.com>
2022-11-22 14:16:43 +0100

    adding comments

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' // importation des composants react-router-dom
 
 import Home from './pages/Home'
 import About from './pages/About'
 import Housing from './pages/Housing'
 import Error from './pages/Error'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render( // intégration des composants REACT dans le DOM
     <React.StrictMode>
-        <Router>
+        <Router> {/* configuration des différentes routes */}
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path='/about' element={<About />}/>
